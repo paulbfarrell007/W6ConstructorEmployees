32fee6d25b3bdb5fd7b448bdd9e91c82179eae5a
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2021-08-23 18:41:54 -0400

    Add files via upload

---
+++ Updated_W6ConstructorEmployees.js
@@ -0,0 +1,33 @@
+var employees = [];
+
+
+function Employee(name, jobTitle, salary, status) {
+    this.name = name
+    this.jobTitle = jobTitle
+    this.salary = salary
+    this.status = "Full Time"
+}
+
+var liam = new Employee("Liam Firestarter", "THE Boss", 880000, "full-time")
+var kevin = new Employee("Kevin Pietersen", "Batsman", 30000, "full-time")
+var lewis = new Employee("Lewis Hamilton", "Track Master", 1000000, "part-time")
+
+
+
+Employee.prototype.printEmployeeForm = function () {
+    console.log(this.name, this.jobTitle, this.salary, this.status)
+}
+
+
+
+
+liam.printEmployeeForm();
+kevin.printEmployeeForm();
+lewis.printEmployeeForm();
+
+lewis.status = "contract";
+
+
+employees.push('liam', 'kevin', 'lewis'); 
+
+console.log(employees)
